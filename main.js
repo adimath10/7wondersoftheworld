ff1546003a53ba1677f385079a04bb12e3b5a6f0
adimath10 <76406048+adimath10@users.noreply.github.com>
2021-01-09 13:38:02 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one() {
+    window.alert("The Great Wall of China is the collective name of a series of fortification systems generally built across the historical northern borders of China to protect and consolidate territories of Chinese states and empires against various nomadic groups of the steppe and their polities.");
+}
+
+function two() {
+    window.alert("Petra is a famous archaeological site in Jordan's southwestern desert.");
+}
+
+function three() {
+    window.alert("The Colosseum or Coliseum also known as the Flavian Amphitheatre is an oval amphitheatre in the centre of the city of Rome, Italy.");
+}
+
+function four() {
+    window.alert("Chichen Itza was a large pre-Columbian city built by the Maya people of the Terminal Classic period.");
+}
+
+function five() {
+    window.alert("Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley.");
+}
+
+function six() {
+    window.alert("The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra.");
+}
+
+function seven() {
+    window.alert("Christ the Redeemer is an Art Deco statue of Jesus Christ in Rio de Janeiro, Brazil, created by French sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot. Romanian sculptor Gheorghe Leonida fashioned the face.");
+}
